d42ff8fd49c9b08373eef722fbd00c5dd207116b
dwlee <wodbow12@gmail.com>
2025-07-16 13:47:37 +0900

    update localhost

--- packages/web/public/env.js
+++ packages/web/public/env.js
@@ -1,4 +1,4 @@
 window.omnivoreEnv = {
-  SERVER_BASE_URL: "{{SERVER_BASE_URL}}",
-  BASE_URL: "{{BASE_URL}}"
+  SERVER_BASE_URL: "http://192.168.10.182:4000",
+  BASE_URL: "http://192.168.10.182:3000"
 }
